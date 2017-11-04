3e93b0629f3abacb0afd72c238499d517964e642
David Stone <dstone@tracegenomics.com>
2017-11-04 20:52:14 +0000

    Adding reqs file, shell for scraping script, and constants file

---
+++ constants.py
@@ -0,0 +1,7 @@
+"""
+Any variables or constants used elsewhere in the cloud atlas
+"""
+
+# the 10 types of tropospheric cloud genera (taken from wiki)
+CLOUD_TYPES = ['stratus', 'stratocumulus', 'cumulus', 'cumulonimbus', 'nimbostratus', 
+               'altostratus', 'altocumulus', 'cirrostratus', 'cirrus', 'cirrocumulus']
---
+++ scrape_images.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+A script to acquire images and store them according to a set of labels provided by the user
+"""
+
+import argparse
+import BeautifulSoup
+import urllib2
+# etc
